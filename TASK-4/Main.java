51873f6bbb7bc629e1563a207fa124139db88851
sahildev75 <siddiquisahil735@gmail.com>
2024-10-14 11:44:43 -0700

    Delete TASK-4 directory

--- TASK-4/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BankAccount account = new BankAccount(1000.00); 
-        ATM atm = new ATM(account);
-        atm.start();
-    }
-}
